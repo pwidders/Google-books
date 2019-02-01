2fb644fe768e343df46d51dbb1f93b53df1f81b5
Pete Widders <pwidders@hotmail.com>
2019-01-31 22:19:38 -0800

    Updates

---
+++ precache-manifest.83383dd5d3f686ccfbb52fe99bb8f88d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "87fd0e23e0ebc74ef227",
+    "url": "/static/js/main.87fd0e23.chunk.js"
+  },
+  {
+    "revision": "48b690652ca053f0a3f4",
+    "url": "/static/js/1.48b69065.chunk.js"
+  },
+  {
+    "revision": "87fd0e23e0ebc74ef227",
+    "url": "/static/css/main.d2d689fc.chunk.css"
+  },
+  {
+    "revision": "0f4f543c205928367e3800a9dcc4817d",
+    "url": "/index.html"
+  }
+];
